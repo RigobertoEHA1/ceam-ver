812b5d6605c0cdfe8580c6e799845fc62fe09d20
Rigoberto Hernandez <142679865+RigobertoEHA1@users.noreply.github.com>
2026-05-05 00:13:53 -0600

    asd

--- app/api/generar-pdf/route.ts
+++ app/api/generar-pdf/route.ts
@@ -224,7 +224,7 @@ export async function POST(req: Request) {
 
     const pdfBytes = await pdfDoc.save();
 
-    return new NextResponse(pdfBytes, {
+    return new NextResponse(Buffer.from(pdfBytes), {
       headers: {
         "Content-Type": "application/pdf",
         "Content-Disposition": `attachment; filename="Constancia_${constancia.curp}.pdf"`,
